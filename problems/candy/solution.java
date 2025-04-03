9df74d9634038985b9a84f39056674646e01f9d8
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-04-03 08:23:37 +0000

    Sync LeetCode submission - Candy (java)

---
+++ problems/candy/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] arr=new int[n];
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                arr[i]=Math.max(arr[i+1]+1,arr[i]);
+            }
+        }
+        int ans=n;
+        for(int i=0;i<n;i++){
+            ans+=arr[i];
+        }
+        return ans;
+    }
+}
