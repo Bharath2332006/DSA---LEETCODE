985d416061a6a94df74646b560067afc5a0c0f0c
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-06-10 06:58:46 +0000

    Sync LeetCode submission - Maximum Difference Between Even and Odd Frequency I (java)

--- problems/maximum_difference_between_even_and_odd_frequency_i/solution.java
+++ problems/maximum_difference_between_even_and_odd_frequency_i/solution.java
@@ -1,24 +1,16 @@
 class Solution {
     public int maxDifference(String s) {
-        int[] f = new int[26] ; 
-        int odd_max = 1 ; 
-        int odd_min = Integer.MAX_VALUE  ;
-        int even_max = 1 ;
-        int even_min = Integer.MAX_VALUE ; 
-        for( char c : s.toCharArray() ){
-            f[ c - 'a' ]++ ; 
+        int count[]=new int[26];
+        int max=0,min=s.length();
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
         }
-        for( int i=0 ; i<26 ; ++i ){
-            if( f[i] % 2 == 1 ){
-                odd_max = Math.max( odd_max , f[i] ) ; 
-                odd_min = Math.min( odd_min , f[i] ) ; 
-
-            }else if( f[i] > 0 ){
-                even_max = Math.max( even_max , f[i] ) ; 
-                even_min = Math.min( even_min , f[i] ) ; 
+        for(int i=0;i<26;i++){
+            if(count[i]%2!=0){
+                max=Math.max(max,count[i]);
+            }if(count[i]%2==0 && count[i]>0){
+                min=Math.min(min,count[i]);
             }
-        }
-        int rs =  odd_max - even_min ; 
-        return rs ; 
+        }return max-min;
     }
 }
