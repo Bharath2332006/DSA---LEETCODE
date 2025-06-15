9f012a0a212b28fcf14fc253d23fdc303a4b00e8
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-06-15 14:14:04 +0000

    Sync LeetCode submission - Max Difference You Can Get From Changing an Integer (java)

---
+++ problems/max_difference_you_can_get_from_changing_an_integer/solution.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int maxDiff(int num) {
+        int l=(int)(Math.log10(num)+1);
+        int arr[]=new int[l];
+        int len=l-1;
+        while(num>0){
+            int n=num%10;
+            arr[len--]=n;
+            num/=10;
+        }
+        int nums1[]=Arrays.copyOf(arr,l);
+        int nums2[]=Arrays.copyOf(arr,l);
+        int k=0;
+        int max=0;
+        for(int i=0;i<l;i++){
+            if(nums1[i]<9) {
+            k=nums1[i];
+            break;
+            }
+        }
+        for(int i=0;i<l;i++){
+            if(nums1[i]==k) nums1[i]=9;
+             
+             max=max*10+nums1[i];
+        
+        }
+       int v = -1, min = 0;
+        if (nums2[0] != 1) {
+            v = nums2[0];  
+            for (int i = 0; i < nums2.length; i++) {
+                if (nums2[i] == v) nums2[i] = 1;
+                min = min * 10 + nums2[i];
+            }
+        } else {
+            for (int i = 1; i < nums2.length; i++) {
+                if (nums2[i] > 1) {
+                v = nums2[i];
+                break;
+                }
+            }
+            if (v == -1) {
+            for (int i = 0; i < nums2.length; i++) {
+                min = min * 10 + nums2[i];
+            }
+            } else {
+                for (int i = 0; i < nums2.length; i++) {
+                if (nums2[i] == v) nums2[i] = 0;
+                min = min * 10 + nums2[i];
+                }
+            }
+        }
+            
+           
+        
+        
+        return (max-min);
+        
+    }
+}
