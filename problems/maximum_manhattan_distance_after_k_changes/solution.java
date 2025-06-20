5e50c7e9c0cf2c5f74f3d3a966e39a4429a65b19
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-06-20 16:30:17 +0000

    Sync LeetCode submission - Maximum Manhattan Distance After K Changes (java)

--- problems/maximum_manhattan_distance_after_k_changes/solution.java
+++ problems/maximum_manhattan_distance_after_k_changes/solution.java
@@ -1,28 +1,24 @@
 class Solution {
-    public int maxDistance(String s, int k) {
-        int n=0 ,so=0,e=0,w=0;
-        int max=0;
-
-        for(char c:s.toCharArray()){
-            if(c=='N'){
+    public int maxDistance(String st, int k) {
+        int n = 0, s = 0, e = 0, w = 0;
+        int max = 0;
+       
+        for (char c : st.toCharArray()) {
+            if (c == 'N') {
                 n++;
-            }
-             if(c=='S'){
-                so++;
-            }
-             if(c=='E'){
+            } else if (c == 'S') {
+                s++;
+            } else if (c == 'E') {
                 e++;
-            }
-             if(c=='W'){
+            } else {
                 w++;
             }
-        
-        max=Math.max(max,n+e-w-so+2 * Math.min(so+w,k));
-         max=Math.max(max,n+w-so-e+2 * Math.min(so+e,k));
-         max=Math.max(max,so+e-n-w+2 * Math.min(n+w,k));
-         max=Math.max(max,so+w-n-e+2 * Math.min(n+e,k));
+            max = Math.max(max, n + e - w - s + 2 * Math.min(s + w, k));
+            max = Math.max(max, n + w - s - e + 2 * Math.min(s + e, k));
+            max = Math.max(max, s + e - n - w + 2 * Math.min(n + w, k));
+            max = Math.max(max, s + w - n - e + 2 * Math.min(n + e, k));
         }
+
         return max;
-        
     }
 }
