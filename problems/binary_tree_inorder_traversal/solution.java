6abde732c31ab9e57fd29c23af0316684324a08f
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-07-28 06:50:04 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (java)

---
+++ problems/binary_tree_inorder_traversal/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> arr=new ArrayList<>();
+        recin(root,arr);
+        return arr;
+
+    }
+    static void recin(TreeNode root,List<Integer> arr){
+        if(root!=null){
+            recin(root.left,arr);
+            arr.add(root.val);
+            recin(root.right,arr);
+        }
+    }
+}
