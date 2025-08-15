62a6f5e6005a1d16f8ff0bc923655aa133e28ac3
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-08-15 14:23:58 +0000

    Sync LeetCode submission - Power of Four (java)

--- problems/power_of_four/solution.java
+++ problems/power_of_four/solution.java
@@ -1,7 +1,6 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        return (n&n-1)==0 && (n-1)%3==0;
-        
-         
-    }
+        public boolean isPowerOfFour(int n) {
+                return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+                    }
+                    
 }
