e9f4944f91ad12e47f98a0f299a039b022733e20
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-08-19 19:19:27 +0000

    Sync LeetCode submission - Sqrt(x) (java)

---
+++ problems/sqrt(x)/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+       int l=1;
+       int r=x;
+       int ans=0;
+       while(l<=r){
+        int mid=(l+r)/2;
+        if(x/mid==mid) return mid;
+        else if(x/mid<mid) r=mid-1;
+        else{
+            ans=mid;
+            l=mid+1;
+        }
+
+       }return ans;
+    }
+}
