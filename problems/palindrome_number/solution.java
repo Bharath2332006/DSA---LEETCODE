a90a2d71bc3feee0369a1e9d103a44c9d4b9de17
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2025-08-27 17:53:12 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- problems/palindrome_number/solution.java
+++ problems/palindrome_number/solution.java
@@ -1,20 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
-     int a=Math.abs(x);
-     boolean ba=false;
-     int s=0;
-     while(a>0){
-       int b=a%10;
-       s+=b;
-       a/=10;
-       if(a>0){
-        s*=10;
-       }
-    }  
-    if(s==x){
-        ba=true;
-    }
-     
-     return ba;
+        int a=x,b=0;
+        while(a>0){
+            b=(b*10)+(a%10);
+            a/=10;
+        }
+        return x==b;
     }
 }
