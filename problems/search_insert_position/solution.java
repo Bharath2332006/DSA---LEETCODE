da6ee15e7a4541a6516da06b1806f088d75923d6
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2026-01-05 09:48:57 +0000

    Sync LeetCode submission - Search Insert Position (java)

--- problems/search_insert_position/solution.java
+++ problems/search_insert_position/solution.java
@@ -1,19 +1,15 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] > target)
-                end = mid - 1;
-            else
-                start = mid + 1;
-
+            if(nums[mid]==target) return mid;
+            if(target<nums[mid]) r=mid-1;
+            else l=mid+1;
         }
-        return start;
-
+        return l;
+        
+        
     }
 }
