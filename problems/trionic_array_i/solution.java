6efcb84dcdf7298133117f49cc38ae7bbbe167ad
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2026-02-04 03:38:21 +0000

    Sync LeetCode submission - Trionic Array I (java)

---
+++ problems/trionic_array_i/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        if(nums.length<3) return false;
+        if(nums[0]>=nums[1]) return false;
+
+        int count=1;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i-1]==nums[i]) return false;
+
+            if((nums[i-2]-nums[i-1])*(nums[i-1]-nums[i])<0) count++;
+        }
+        return count==3;
+    }
+}
