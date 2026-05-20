22116543775280c36f92dc0a0e6ee54f7f2c76b6
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2026-05-20 08:32:41 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- problems/valid_anagram/solution.java
+++ problems/valid_anagram/solution.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer>map=new HashMap<>();
+        if(s.length()!=t.length()) return false;
+        Map<Character,Integer>map=new HashMap<>();
         for(char c:s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
         for(char c:t.toCharArray()){
             map.put(c,map.getOrDefault(c,0)-1);
-
             if(map.get(c)==0) map.remove(c);
         }
-
-        return map.size()!=0?false:true;
+        return map.size()==0;
     }
 }
