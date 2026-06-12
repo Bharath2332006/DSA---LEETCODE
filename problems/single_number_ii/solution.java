d7231f91b43b8d4a4dd3edd147d6590d23c63229
BHARATH M <167199125+Bharath2332006@users.noreply.github.com>
2026-06-12 15:48:38 +0000

    Sync LeetCode submission - Single Number II (java)

---
+++ problems/single_number_ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        int num=0;
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue()==1) {
+                num=entry.getKey();
+                break;
+            }
+
+        }
+        return num;
+    }
+}
